63ef6009202c3631f5d8e81be197d73cd329cae3
Lucas Campos <lucascampos.ads@gmail.com>
2020-09-30 20:07:38 -0300

    feat: Inicia configuracoes para conexao com banco de dados

--- Next-Level-Week01/server/src/server.ts
+++ Next-Level-Week01/server/src/server.ts
@@ -1,40 +1,9 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
-const users = ["Lucas", "Diego", "Cleiton", "Daniel", "Robson"];
-
-app.get("/users", (request, response) => {
-  const search = String(request.query.search);
-
-  const filteredUsers = search
-    ? users.filter((user) => user.includes(search))
-    : users;
-
-  return response.json(filteredUsers);
-});
-
-app.get("/users/:id", (request, response) => {
-  const id = Number(request.params.id);
-
-  const user = users[id];
-
-  return response.json(user);
-});
-
-app.post("/users", (request, response) => {
-  const data = request.body;
-
-  console.log(data);
-  
-  const user = {
-    name: data.name,
-    email: data.email,
-  };
-
-  return response.json(user);
-});
-
-app.listen(3333);
+app.listen(3333); // 37:00
