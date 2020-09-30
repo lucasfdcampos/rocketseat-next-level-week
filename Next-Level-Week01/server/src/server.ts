2e91f7626c88f3313174831f518ecc565d21758d
Lucas Campos <lucascampos.ads@gmail.com>
2020-09-29 23:03:40 -0300

    feat: Inicia metodos http

--- Next-Level-Week01/server/src/server.ts
+++ Next-Level-Week01/server/src/server.ts
@@ -1,19 +1,40 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('Listagem de usuários');
+app.use(express.json());
 
+const users = ["Lucas", "Diego", "Cleiton", "Daniel", "Robson"];
+
+app.get("/users", (request, response) => {
+  const search = String(request.query.search);
+
+  const filteredUsers = search
+    ? users.filter((user) => user.includes(search))
+    : users;
+
+  return response.json(filteredUsers);
+});
+
+app.get("/users/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  const user = users[id];
+
+  return response.json(user);
+});
+
+app.post("/users", (request, response) => {
+  const data = request.body;
+
+  console.log(data);
   
+  const user = {
+    name: data.name,
+    email: data.email,
+  };
 
-  // JSON
-  response.json([
-    'Lucas',
-    'Diego',
-    'Cleiton',
-    'Daniel',
-  ]);
+  return response.json(user);
 });
 
 app.listen(3333);
