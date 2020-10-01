fa501bb92d4efd8227d73e25a255388df90ac21f
Lucas Campos <lucascampos.ads@gmail.com>
2020-09-30 22:43:23 -0300

    doc: Adicionado imagens estaticas de itens de coleta

--- Next-Level-Week01/server/src/routes.ts
+++ Next-Level-Week01/server/src/routes.ts
@@ -2,8 +2,8 @@ import express from 'express';
 
 const routes = express.Router();
 
-routes.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
+routes.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' });
 });
 
 export default routes;
