63e1e84d646cef933ab2520026c6e5a893979966
Lucas Campos <lucascampos.ads@gmail.com>
2020-10-02 18:51:40 -0300

    feat: Inicia HTML do projeto

--- Next-Level-Week01/web/src/App.tsx
+++ Next-Level-Week01/web/src/App.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import './App.css';
 
-import Header from './Header';
-
 function App() {
   return (
-    <div>
-      <Header title="Hello World" />
-
-      <h1>Conteúdo da aplicação</h1>
-    </div>
+    <h1>Hello World</h1>
   );
 }
 
