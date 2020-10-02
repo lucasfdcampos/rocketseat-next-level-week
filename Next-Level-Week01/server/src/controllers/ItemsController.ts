3aae349d1a44d50504f095005faa64c3734aef98
Lucas Campos <lucascampos.ads@gmail.com>
2020-10-02 14:57:01 -0300

    docs: change Prettier to single Quote

--- Next-Level-Week01/server/src/controllers/ItemsController.ts
+++ Next-Level-Week01/server/src/controllers/ItemsController.ts
@@ -1,9 +1,9 @@
-import { Request, Response } from "express";
-import knex from "../database/connection";
+import { Request, Response } from 'express';
+import knex from '../database/connection';
 
 class ItemsController {
   async index(request: Request, response: Response) {
-    const items = await knex("items").select("*");
+    const items = await knex('items').select('*');
 
     const serializedItems = items.map((item) => {
       return {
