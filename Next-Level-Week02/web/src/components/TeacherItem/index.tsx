ca488c22ccc5775efb00cde041364f8919902298
Lucas Campos <lucascampos.ads@gmail.com>
2020-10-06 23:15:04 -0300

    feat: Cria componente TeacherItem

---
+++ Next-Level-Week02/web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars3.githubusercontent.com/u/56896330?s=460&u=ec2f0df0fa70e102fd7ad8c0e1db31a2490ad0dd&v=4"
+          alt="Lucas Campos"
+        />
+        <div>
+          <strong>Lucas Campos</strong>
+          <span>Química</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br />
+        <br />
+        Apaixonado por explor coisas em laboratório e por mudar a vida das
+        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
+        uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 80,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
