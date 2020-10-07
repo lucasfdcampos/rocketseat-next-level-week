f46731999d88fcf2fda12e5e22df79118fc83c50
Lucas Campos <lucascampos.ads@gmail.com>
2020-10-06 22:35:13 -0300

    feat: Cria page TeacherList

--- Next-Level-Week02/web/src/pages/TeacherList/index.tsx
+++ Next-Level-Week02/web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,68 @@
 import React from 'react';
 
+import PageHeader from '../../components/PageHeader';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
 function TeacherList() {
   return (
-    <h1>Teachers</h1>
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os proffys disponíveis.">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subject" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="week-day">Dia da semana</label>
+            <input type="text" id="week-day" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="time">Hora</label>
+            <input type="text" id="time" />
+          </div>
+        </form>
+      </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img
+              src="https://avatars3.githubusercontent.com/u/56896330?s=460&u=ec2f0df0fa70e102fd7ad8c0e1db31a2490ad0dd&v=4"
+              alt="Lucas Campos"
+            />
+            <div>
+              <strong>Lucas Campos</strong>
+              <span>Química</span>
+            </div>
+          </header>
+
+          <p>
+            Entusiasta das melhores tecnologias de química avançada.
+            <br />
+            <br />
+            Apaixonado por explor coisas em laboratório e por mudar a vida das
+            pessoas através de experiências. Mais de 200.000 pessoas já passaram
+            por uma das minhas explosões.
+          </p>
+
+          <footer>
+            <p>
+              Preço/hora
+              <strong>R$ 80,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="Whatsapp"/>
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+      </main>
+    </div>
   );
 }
 
