590eed99220ca5cf9bc3a31c1014437a077f7126
Lucas Campos <lucascampos.ads@gmail.com>
2020-10-09 20:40:47 -0300

    feat: Altera campos incluindo nos formularios os componentes de select e textarea

--- Next-Level-Week02/web/src/pages/TeacherForm/index.tsx
+++ Next-Level-Week02/web/src/pages/TeacherForm/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
+import Textarea from '../../components/TextArea';
+import Select from '../../components/Select';
 
 import warningIncon from '../../assets/images/icons/warning.svg';
 
@@ -22,12 +24,23 @@ function TeacherForm() {
           <Input name="name" label="Nome completo" />
           <Input name="avatar" label="Avatar" />
           <Input name="whatsapp" label="Whatsapp" />
+          <Textarea name="bio" label="Biografia" />
         </fieldset>
 
         <fieldset>
           <legend>Sobre a aula</legend>
 
-          <Input name="subject" label="Matéria" />
+          <Select name="subject" label="Matéria" options={[
+            { value: 'Artes', label: 'Artes' },
+            { value: 'Biologia', label: 'Biologia' },
+            { value: 'Educação física', label: 'Educação física' },
+            { value: 'Física', label: 'Física' },
+            { value: 'Geografia', label: 'Geografia' },
+            { value: 'História', label: 'História' },
+            { value: 'Matemática', label: 'Matemática' },
+            { value: 'Português', label: 'Português' },
+            { value: 'Química', label: 'Química' },
+          ]} />
           <Input name="cost" label="Custo da sua hora por aula" />
         </fieldset>
 
