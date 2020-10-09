07d85ee6d4c5032886653b51d7686d066900f44c
Lucas Campos <lucascampos.ads@gmail.com>
2020-10-08 23:55:41 -0300

    feat: Altera cabecalho

--- Next-Level-Week02/web/src/components/PageHeader/index.tsx
+++ Next-Level-Week02/web/src/components/PageHeader/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-import logoImg from '../../assets/images/logo.svg';
-import backIcon from '../../assets/images/icons/back.svg';
+import { Link } from 'react-router-dom'
+
+import logoImg from '../../assets/images/logo.svg'
+import backIcon from '../../assets/images/icons/back.svg'
 
 import './styles.css';
 
 interface PageHeaderProps {
   title: string;
+  description?: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
@@ -15,18 +17,19 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
     <header className="page-header">
       <div className="top-bar-container">
         <Link to="/">
-          <img src={backIcon} alt="Voltar" />
+          <img src={backIcon} alt="voltar" />
         </Link>
         <img src={logoImg} alt="Proffy" />
       </div>
 
       <div className="header-content">
         <strong>{props.title}</strong>
+        {props.description && <p>{props.description}</p>}
 
         {props.children}
-      </div>      
+      </div>
     </header>
-  );
-};
+  )
+}
 
 export default PageHeader;
