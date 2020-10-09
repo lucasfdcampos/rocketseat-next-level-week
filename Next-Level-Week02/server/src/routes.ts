8ab938916477bdfc32d692ed52e4ed16f02f999c
Lucas Campos <lucascampos.ads@gmail.com>
2020-10-08 22:01:59 -0300

    feat: Cria controle de conexoes

--- Next-Level-Week02/server/src/routes.ts
+++ Next-Level-Week02/server/src/routes.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import ClassesController from './controllers/ClassesController';
+import ConnectionsControler from './controllers/ConnectionsController';
 
 const routes = express.Router();
 const classesController = new ClassesController();
+const connectionsController = new ConnectionsControler();
 
 routes.post('/classes', classesController.create);
 routes.get('/classes', classesController.index);
+
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
 
 export default routes;
