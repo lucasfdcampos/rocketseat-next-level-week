26b5f2c113df896ae9542aefee2903a44d343eab
Lucas Campos <lucascampos.ads@gmail.com>
2020-10-13 20:56:44 -0300

    Add Sqlite3 and TypeOrm

--- Next-Level-Week03/backend/src/server.ts
+++ Next-Level-Week03/backend/src/server.ts
@@ -2,8 +2,10 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', () => {
-  console.log('Hello World');
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' });
 });
 
 app.listen(3333);
