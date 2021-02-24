dbf73da6c3841ba8fae2ac193aa963508deacab3
Lucas Campos <lucascampos.ads@gmail.com>
2021-02-23 22:19:05 -0300

    feat: Add CompletedChallenges component

--- Next-Level-Week04/moveit-next/src/pages/index.tsx
+++ Next-Level-Week04/moveit-next/src/pages/index.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { CompletedChallenges } from '../components/CompletedChallenges';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
 
@@ -11,6 +13,7 @@ export default function Home() {
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />          
         </div>
         <div>
 
