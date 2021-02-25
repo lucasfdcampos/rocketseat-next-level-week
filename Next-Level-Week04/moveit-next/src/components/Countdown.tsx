7ca953fba3f9249d2f9a8f27bc00e64b1938ec1b
Lucas Campos <lucascampos.ads@gmail.com>
2021-02-25 18:16:17 -0300

    refactor: ident countdown component

--- Next-Level-Week04/moveit-next/src/components/Countdown.tsx
+++ Next-Level-Week04/moveit-next/src/components/Countdown.tsx
@@ -55,7 +55,9 @@ export function Countdown() {
       </div>
 
       {hasFinished ? (
-        <button disabled className={styles.countdownButton}>
+        <button 
+          disabled 
+          className={styles.countdownButton}>
           Ciclo encerrado
         </button>
       ) : (
