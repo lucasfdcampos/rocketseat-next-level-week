82d6cf3edba111108179223c98a4a4ea189ac0fc
Lucas Campos <lucascampos.ads@gmail.com>
2021-07-04 12:41:07 -0300

    Add assets and SignIn page

---
+++ Next-Level-Week06/gameplay/src/screens/SignIn/index.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput } from 'react-native';
+
+import { styles } from './styles';
+
+export function SignIn() {
+  const [text, setText] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <Text> Hello World, page SignIn.</Text>
+
+      <TextInput style={styles.input} onChangeText={setText} />
+
+      <Text>Você digitou: {text} </Text>
+    </View>
+  );
+}
