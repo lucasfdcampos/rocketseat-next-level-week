0934ca0268d1120d072ce93bce5adb75f61bda0d
Lucas Campos <lucascampos.ads@gmail.com>
2021-07-10 18:35:24 -0300

    feat: Add ButtonAdd, Category and Category select components

--- Next-Level-Week06/gameplay/src/components/ButtonAdd/styles.ts
+++ Next-Level-Week06/gameplay/src/components/ButtonAdd/styles.ts
@@ -1,17 +1,13 @@
 import { StyleSheet } from 'react-native';
+import { theme } from '../../global/styles/theme';
 
 export const styles = StyleSheet.create({
   container: {
-    width: 48,
     height: 48,
+    width: 48,
+    backgroundColor: theme.colors.primary,
     borderRadius: 8,
     alignItems: 'center',
     justifyContent: 'center',
-    marginRight: 22,
-  },
-  avatar: {
-    width: 45,
-    height: 45,
-    borderRadius: 8,
   },
 });
--- Next-Level-Week06/gameplay/src/screens/SignIn/index.tsx
+++ Next-Level-Week06/gameplay/src/screens/SignIn/index.tsx
@@ -34,7 +34,6 @@ export function SignIn() {
 
         <ButtonIcon
           title="Entrar com Discord"
-          activeOpacity={0.7}
           onPress={handleSignIn}
         />
       </View>
