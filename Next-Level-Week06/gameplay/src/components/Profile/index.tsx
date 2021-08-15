faa78f6ee58fa26e4ba107183ba25d75062ed996
Lucas Campos <lucascampos.ads@gmail.com>
2021-08-14 21:07:01 -0300

    feat: Show user data

--- Next-Level-Week06/gameplay/src/components/Profile/index.tsx
+++ Next-Level-Week06/gameplay/src/components/Profile/index.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import { useAuth } from '../../hooks/auth';
+
 import { Avatar } from '../Avatar';
 import { styles } from './styles';
 
 export function Profile() {
+  const { user } = useAuth();
+
   return (
     <View style={styles.container}>
-      <Avatar urlImage="https://github.com/lucasfdcampos.png" />
+      <Avatar urlImage={user.avatar} />
 
       <View>
         <View style={styles.user}>
           <Text style={styles.greeting}>Ola,</Text>
 
-          <Text style={styles.username}>Lucas</Text>
+          <Text style={styles.username}>{user.firstName}</Text>
         </View>
 
         <Text style={styles.message}>Hoje é dia de vitória</Text>
