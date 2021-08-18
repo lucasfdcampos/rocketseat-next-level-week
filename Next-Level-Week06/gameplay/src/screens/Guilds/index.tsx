85da699a1df9156a465704af21f43f967c07cbf2
Lucas Campos <lucascampos.ads@gmail.com>
2021-08-18 20:43:37 -0300

    feat: Get guilds from server

--- Next-Level-Week06/gameplay/src/screens/Guilds/index.tsx
+++ Next-Level-Week06/gameplay/src/screens/Guilds/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, FlatList } from 'react-native';
-import { GuildProps } from '../../components/Guild';
 
-import { Guild } from '../../components/Guild';
+import { Guild, GuildProps } from '../../components/Guild';
 import { ListDivider } from '../../components/ListDivider';
+import { Load } from '../../components/Load';
+
+import { api } from '../../services/api';
 
 import { styles } from './styles';
 
@@ -12,35 +14,38 @@ type Props = {
 };
 
 export function Guilds({ handleGuildSelect }: Props) {
-  const guilds = [
-    {
-      id: '1',
-      name: 'Lendários',
-      icon: null,
-      owner: true,
-    },
-    {
-      id: '2',
-      name: 'Orda',
-      icon: null,
-      owner: true,
-    },
-  ];
+  const [guilds, setGuilds] = useState<GuildProps[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchGuilds() {
+    const response = await api.get('/users/@me/guilds');
+
+    setGuilds(response.data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchGuilds();
+  }, []);
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={guilds}
-        keyExtractor={(item) => item.id}
-        contentContainerStyle={{ paddingBottom: 68, paddingTop: 103 }}
-        ListHeaderComponent={() => <ListDivider isCentered />}
-        ItemSeparatorComponent={() => <ListDivider isCentered />}
-        renderItem={({ item }) => (
-          <Guild data={item} onPress={() => handleGuildSelect(item)} />
-        )}
-        showsVerticalScrollIndicator={false}
-        style={styles.guilds}
-      />
+      {loading ? (
+        <Load />
+      ) : (
+        <FlatList
+          data={guilds}
+          keyExtractor={(item) => item.id}
+          contentContainerStyle={{ paddingBottom: 68, paddingTop: 103 }}
+          ListHeaderComponent={() => <ListDivider isCentered />}
+          ItemSeparatorComponent={() => <ListDivider isCentered />}
+          renderItem={({ item }) => (
+            <Guild data={item} onPress={() => handleGuildSelect(item)} />
+          )}
+          showsVerticalScrollIndicator={false}
+          style={styles.guilds}
+        />
+      )}
     </View>
   );
 }
