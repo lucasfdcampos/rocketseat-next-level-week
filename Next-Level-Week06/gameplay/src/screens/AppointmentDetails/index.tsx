dea5ff3f11fbdf95f0cf54ca1043820632fb67af
Lucas Campos <lucascampos.ads@gmail.com>
2021-08-20 20:05:39 -0300

    feat: Load appointment details and counting players

--- Next-Level-Week06/gameplay/src/screens/AppointmentDetails/index.tsx
+++ Next-Level-Week06/gameplay/src/screens/AppointmentDetails/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import { ImageBackground, View, Text, FlatList } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { ImageBackground, View, Text, FlatList, Alert } from 'react-native';
 import { Fontisto } from '@expo/vector-icons';
 import { BorderlessButton } from 'react-native-gesture-handler';
 import { useRoute } from '@react-navigation/native';
 
 import { AppointmentsProps } from '../../components/Appointment';
+import { Member, MemberProps } from '../../components/Member';
 import { ListDivider } from '../../components/ListDivider';
 import { Background } from '../../components/Background';
 import { ListHeader } from '../../components/ListHeader';
 import { ButtonIcon } from '../../components/ButtonIcon';
 import { Header } from '../../components/Header';
-import { Member } from '../../components/Member';
+import { Load } from '../../components/Load';
 
 import { theme } from '../../global/styles/theme';
 import BannerImg from '../../assets/banner.png';
+
+import { api } from '../../services/api';
 
 import { styles } from './styles';
 
@@ -21,24 +24,36 @@ type Params = {
   guildSelected: AppointmentsProps;
 };
 
+type GuildWidget = {
+  id: string;
+  name: string;
+  instant_invite: string;
+  members: MemberProps[];
+};
+
 export function AppointmentDetails() {
+  const [widget, setWidget] = useState<GuildWidget>({} as GuildWidget);
+  const [loading, setLoading] = useState(true);
+
   const route = useRoute();
   const { guildSelected } = route.params as Params;
 
-  const members = [
-    {
-      id: '1',
-      username: 'Lucas',
-      avatar_url: 'https://github.com/lucasfdcampos.png',
-      status: 'online',
-    },
-    {
-      id: '2',
-      username: 'Rodrigo',
-      avatar_url: 'https://github.com/rodrigorgtic.png',
-      status: 'offline',
-    },
-  ];
+  async function fetchGuildWidget() {
+    try {
+      const response = await api.get(
+        `/guilds/${guildSelected.guild.id}/widget.json`
+      );
+      setWidget(response.data);
+    } catch {
+      Alert.alert('Verifique as configurações do servidor.');
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchGuildWidget();
+  }, []);
 
   return (
     <Background>
@@ -58,15 +73,24 @@ export function AppointmentDetails() {
         </View>
       </ImageBackground>
 
-      <ListHeader title="Jogadores" subtitle="Total 3" />
+      {loading ? (
+        <Load />
+      ) : (
+        <>
+          <ListHeader
+            title="Jogadores"
+            subtitle={`Total de ${widget.members.length}`}
+          />
 
-      <FlatList
-        data={members}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Member data={item} />}
-        ItemSeparatorComponent={() => <ListDivider isCentered />}
-        style={styles.members}
-      />
+          <FlatList
+            data={widget.members}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => <Member data={item} />}
+            ItemSeparatorComponent={() => <ListDivider isCentered />}
+            style={styles.members}
+          />
+        </>
+      )}
 
       <View style={styles.footer}>
         <ButtonIcon title="Entrar na partida" />
